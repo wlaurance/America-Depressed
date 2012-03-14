2933b09a11b35d33f40c25dad8e2b3018ca1c22d
wlaurance <w.laurance@gmail.com>
2012-03-13 21:51:25 -0400

    init server
    responds with 501 not implemented

--- server/coffee/server.coffee
+++ server/coffee/server.coffee
@@ -1,2 +1,20 @@
 http = require 'http'
 winston = require 'winston'
+
+exports.createServer = (port)->
+  server = http.createServer (request, response) =>
+    data = ''
+    winston.info 'Incoming Request', { url: request.url }
+
+    request.on 'data', (chunk)->
+        data += chunk
+
+    response.writeHead 501, { 'Content-Type': 'application/json' }
+    response.end JSON.stringify { message: 'not implemented' }
+  if port
+    server.listen port
+  server
+
+exports.start = (options, callback) ->
+  server = exports.createServer options.port
+  callback null, server
--- server/lib/server.js
+++ server/lib/server.js
@@ -5,4 +5,33 @@
 
   winston = require('winston');
 
+  exports.createServer = function(port) {
+    var server,
+      _this = this;
+    server = http.createServer(function(request, response) {
+      var data;
+      data = '';
+      winston.info('Incoming Request', {
+        url: request.url
+      });
+      request.on('data', function(chunk) {
+        return data += chunk;
+      });
+      response.writeHead(501, {
+        'Content-Type': 'application/json'
+      });
+      return response.end(JSON.stringify({
+        message: 'not implemented'
+      }));
+    });
+    if (port) server.listen(port);
+    return server;
+  };
+
+  exports.start = function(options, callback) {
+    var server;
+    server = exports.createServer(options.port);
+    return callback(null, server);
+  };
+
 }).call(this);
