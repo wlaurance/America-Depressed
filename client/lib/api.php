dc1070b5d9a17660fd9c050d3d09a937df29d652
wlaurance <w.laurance@gmail.com>
2012-04-13 14:12:59 -0400

    login functionallity
    for client api

--- client/lib/api.php
+++ client/lib/api.php
@@ -2,7 +2,7 @@
 
 $url = 'http://bg6:8000';
 echo getTime();
-
+echo login('wlaurance', 'password123');
 function getTime() 
 {
   global $url;
@@ -14,5 +14,19 @@ function getTime()
   curl_close($ch);
   $time = json_decode($result);
   return $time->{'hello'};
+}
+
+function login($username, $password)
+{
+  global $url;
+  $route = $url . '/login';
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $route);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, "username={$username}&password={$password}");
+  $result = curl_exec($ch);
+  curl_close($ch);
+  $sessionid = json_decode($result);
+  return $sessionid->{'token'};
 }
 ?>
